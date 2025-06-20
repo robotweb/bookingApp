e184a2d14e7e581fe3f4fa304ca676bf632c89ae
jaredleechrstians <jaredleechristians@gmail.com>
2025-06-20 23:06:28 +0200

    feat: integrate nuxt-app into main repository

--- nuxt-app/netlify/functions/api.js
+++ nuxt-app/netlify/functions/api.js
@@ -1,4 +1,4 @@
 const serverlessExpress = require('@vendia/serverless-express');
-const app = require('./express-app/server');
+const app = require('../../../express-app/src/app');
 
 exports.handler = serverlessExpress({ app }); 
